70bbc5f22a82966848e3f9b4f17f0dd493ef9a6c
John Robinson <john@bayfrontmedia.com>
2023-08-18 00:59:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+## Monolog PDO
+
+[Monolog](https://github.com/Seldaek/monolog) handler used to write log files to a MySQL database.
+
+- [License](#license)
+- [Author](#author)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
+
+## Author
+
+<img src="https://cdn1.onbayfront.com/bfm/brand/bfm-logo.svg" alt="Bayfront Media" width="250" />
+
+- [Bayfront Media homepage](https://www.bayfrontmedia.com?utm_source=github&amp;utm_medium=direct)
+- [Bayfront Media GitHub](https://github.com/bayfrontmedia)
+
+## Requirements
+
+* PHP >= 8.0
+* `PDO` PHP extension
+
+## Installation
+
+```
+composer require bayfrontmedia/monolog-pdo
+```
+
+## Usage
+
+Before pushing this handler to a `Logger` instance, you must first create the necessary database table to store the records.
+
+The table can be created using the `up` method, and removed using the `down` method.
+
+The constructor requires a `PDO` instance, and the table name you wish to use:
+
+```php
+use Bayfront\MonologPDO\PDOHandler;
+
+/** @var PDO $pdo */
+$handler = new PDOHandler($pdo, 'table_name');
+
+$handler->up();
+```
+
+Once the table has been created, the handler can be pushed to a `Logger` instance:
+
+```php
+use Bayfront\MonologPDO\PDOHandler;
+use Monolog\Logger;
+
+$log = new Logger('channel_name');
+
+/** @var PDO $pdo */
+$handler = new PDOHandler($pdo, 'table_name');
+
+$log->pushHandler($handler);
+```
+
+From here, your log records should appear in the MySQL table.
---
+++ src/PDOHandler.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Bayfront\MonologPDO;
+
+use Bayfront\ArrayHelpers\Arr;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Level;
+use Monolog\LogRecord;
+use PDO;
+
+class PDOHandler extends AbstractProcessingHandler
+{
+
+    private PDO $pdo;
+    private string $table_name;
+
+    public function __construct(PDO $pdo, string $table_name = 'logs', int|string|Level $level = Level::Debug, bool $bubble = true)
+    {
+        $this->pdo = $pdo;
+        $this->table_name = $table_name;
+        parent::__construct($level, $bubble);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function write(LogRecord $record): void
+    {
+
+        $arr = $record->toArray();
+
+        $sql = "INSERT INTO $this->table_name (channel, level, levelName, message, context, extra, createdAt) VALUES (?,?,?,?,?,?,?)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([
+            $record->channel,
+            Arr::get($arr, 'level'),
+            Arr::get($arr, 'level_name'),
+            $record->message,
+            empty($record->context) ? null : json_encode($record->context),
+            empty($record->extra) ? null : json_encode($record->extra),
+            $record->datetime->format('Y-m-d H:i:s')
+        ]);
+
+    }
+
+    /**
+     * Create database table for handler.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+
+        $query = $this->pdo->prepare("CREATE TABLE IF NOT EXISTS $this->table_name (
+                `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                `channel` VARCHAR(255), 
+                `level` INT(3),
+                `levelName` VARCHAR(10),
+                `message` TEXT,
+                `context` JSON,
+                `extra` JSON,
+                `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci");
+
+        $query->execute();
+
+    }
+
+    /**
+     * Remove database table for handler.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        $query = $this->pdo->prepare("DROP TABLE IF EXISTS $this->table_name");
+        $query->execute();
+    }
+
+}
